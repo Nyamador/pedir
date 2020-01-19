35eb36b7005b20fb6d4961895c7ece7588fad266
Nyamador <dselasi4u@gmail.com>
2020-01-19 14:05:30 +0000

    as

--- pedir/settings.py
+++ pedir/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'iq811rz-*1--jd8z*^lh^yt$72f)36fa)5lmih36^*y+-t7!-6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','https://scribblee.herokuapp.com']
 
 # Application definition
 
@@ -87,13 +87,19 @@ WSGI_APPLICATION = 'pedir.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
