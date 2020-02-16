49c0c6e46d17b9a7e8e490e90bc365fccb09b8c0
Desmond <48738520+Nyamador@users.noreply.github.com>
2020-02-16 20:42:04 +0000

    Update settings.py

--- pedir/settings.py
+++ pedir/settings.py
@@ -101,7 +101,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
     }
 }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
